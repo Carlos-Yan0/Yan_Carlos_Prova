626d70ea5fe3960c9e4e3f415382c50e83580f88
Carlos-Yan0 <yan.c.oliveira08@gmail.com>
2025-08-21 01:30:35 -0300

    Menu dropdown
    Optei por fazer um arquivo com o menu onde posso apenas chamar em outros arquivos

---
+++ sidebar.php
@@ -0,0 +1,74 @@
+<?php
+//OBTENDO O NOME DO PERFIL DO USUARIO LOGADO
+$id_perfil = $_SESSION['perfil'];
+$sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+$stmtPerfil = $pdo->prepare($sqlPerfil);
+$stmtPerfil->bindParam(":id_perfil", $id_perfil);
+$stmtPerfil->execute();
+$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+$nome_perfil = $perfil['nome_perfil'];
+
+//DEFINIÇÃO DAS PERMISSÕES POR perfil
+$permissoes = [
+    //adm
+    1 => ["cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
+
+    "buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
+
+    "alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
+
+    "excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"]],
+
+
+    //secretaria
+    2 => ["cadastrar"=>["cadastro_cliente.php"],
+
+    "buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_usuario.php"],
+
+    "alterar"=>["alterar_fornecedor.php", "alterar_produto.php"],
+
+    "excluir"=>["excluir_produto.php"]],
+
+
+    //almoxarife
+    3 => ["cadastrar"=>["cadastro_fornecedor.php", "cadastro_produto.php"],
+
+    "buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+
+    "alterar"=>["alterar_fornecedor.php", "alterar_produto.php"],
+
+    "excluir"=>["excluir_produto.php"]],
+
+
+    //cliente
+    4 => ["cadastrar"=>["cadastro_cliente.php"],
+
+    "buscar"=>["buscar_produto.php"],
+
+    "alterar"=>["alterar_cliente.php"],
+    ]
+];
+
+//OBTENDO AS OPÇÕES DISPONIVEIS PARA O PERFIL LOGADO
+$opcoes_menu = $permissoes[$id_perfil];
+?>
+
+<nav>
+        <ul class="menu">
+            <li>
+                <a href="principal.php">Home</a>
+            </li>
+            <?php foreach($opcoes_menu as $categoria => $arquivos):  ?>
+                <li class="dropdown">
+                    <a href="#"><?=$categoria?></a>
+                    <ul class="dropdown-menu">
+                        <?php foreach($arquivos as $arquivo): ?>
+                            <li>
+                                <a href="<?=$arquivo ?>"><?= ucfirst(str_replace("_"," ",basename($arquivo, ".php"))) ?></a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
